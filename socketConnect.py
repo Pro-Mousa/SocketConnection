11131392f6b8e300fd00160f81e03e33716c4ab8
Mousa Muigai <mousa@mousa.Mousa>
2026-05-14 21:36:48 +0300

    Socket

--- socketConnect.py
+++ socketConnect.py
@@ -4,6 +4,6 @@ from socket import SOCK_STREAM, socket, AF_INET
 connection = socket(AF_INET, SOCK_STREAM)
 connection.connect(("10.0.2.10", 8080))
 
-connection.send("Connection established")
+connection.send(b"Connection established")
 
 connection.close()
